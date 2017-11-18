cd702a9e78714bf42715672b874fa9b392fc51be
Dan Kleiman <info@dankleiman.com>
2017-11-18 11:38:08 -0500

    removing v1 data

--- tmp/json_parsing.rb
+++ tmp/json_parsing.rb
@@ -8,13 +8,16 @@ root_dir = "/Users/dankleiman/Dropbox/rcrosslinks"
 # - Code
 # - BigQuery
 # ---
+
+# {"subreddit":"093game","cross_linked_subs":[{"xpost_sub":"livven","count":"4"},{"xpost_sub":"SCP","count":"3"},{"xpost_sub":"creepy","count":"1"},{"xpost_sub":"ProCSS","count":"1"},{"xpost_sub":"gaming","count":"1"}]}
 pages = {}
-File.open('top-20-2017.json', 'r').each do |line|
+File.open('top-20-agg.json', 'r').each do |line|
   entry = JSON.parse(line)
   subreddit = entry["subreddit"]
-  pages[subreddit] ||= []
-  pages[subreddit] << entry["xpost_sub"]
+  pages[subreddit] = entry["cross_linked_subs"].map { |xpost_hash| xpost_hash["xpost_sub"] }
 end
+
+puts "PARSED #{pages.keys.size} SUBREDDITS"
 
 pages.each do |subreddit, crosslinks|
   subreddit_dir = root_dir + "/content/pages/#{subreddit}.md"
@@ -26,3 +29,12 @@ pages.each do |subreddit, crosslinks|
   f << '---'
   f.close
 end
+
+puts "CREATED #{pages.keys.size} PAGES"
+
+f = File.new("#{root_dir}/data/subreddits.yaml", 'w')
+f << pages.keys.to_yaml
+f << '---'
+f.close
+
+puts "STORED METADATA FOR #{pages.keys.size} SUBREDDITS"
